76932e695dd85ed3baf125e181e64b3bb75a1f95
littmus <sdw2648@gmail.com>
2013-06-19 13:43:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 
 var SERVER = '192.168.147.139',
 	SERVER_DB_USER = 'root',
-	SERVER_DB_PASSWORD = 'ted705',
+	SERVER_DB_PASSWORD = 'root',
 	SERVER_DB = 'meku';
 
 exports.DB_CONN = mysql.createConnection({
